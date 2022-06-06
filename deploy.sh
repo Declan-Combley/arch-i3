4a1357e9aef189c2dc2ef90a2e8c5b4674d3f412
declan <declan-combley@protonmail.com>
2022-06-06 21:31:55 +0800

    made manual yay install the first thing you do

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-yes | sudo pacman -Syyu && yes | sudo pacman -S zsh tmux rxvt-unicode emacs polybar picom caja bluez bluez-utils pulseaudio-bluetooth
-
 git clone https://aur.archlinux.org/yay-git ~/.config/yay-git && (cd ~/.config/yay-git/ && yes | makepkg -si)
 
+yay -S picom-git
 yes | yay -S ttc-iosevka 
 
-yay -S picom-git
+yes | sudo pacman -Syyu && yes | sudo pacman -S zsh tmux rxvt-unicode emacs polybar picom caja bluez bluez-utils pulseaudio-bluetooth
 
 rm ~/.bashrc
 rm ~/.emacs
