a62e0b89e760ca902834a16278311a4f0cb5405e
declan <declan-combley@protonmail.com>
2022-06-14 18:28:56 +0800

    added dev manpages

--- os/pacman.sh
+++ os/pacman.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-git clone https://aur.archlinux.org/yay-git ~/.config/yay-git && (cd ~/.config/yay-git/ && yes | makepkg -si)
+git clone https://aur.archlinux.org/yay-git ~/.config/yay-git && (cd ~/.config/yay-git/ && yes | makepkg -si) && echo "installed the aur"
 
 yay -S picom-git
 yes | yay -S ttc-iosevka 
 
-yes | sudo pacman -Syyu && yes | sudo pacman -S zsh tmux rxvt-unicode emacs polybar picom caja bluez bluez-utils pulseaudio-bluetooth unzip btop
+yes | sudo pacman -Syyu && yes | sudo pacman -S zsh tmux rxvt-unicode emacs polybar picom caja bluez bluez-utils pulseaudio-bluetooth unzip btop man-pages
 
