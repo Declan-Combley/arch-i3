c32bd0fde62dbb1fcaf9bc8d307b4ceaf325f0c1
declan <declan-combley@protonmail.com>
2022-06-18 16:51:58 +0800

    added rlwrap to config

--- os/pacman.sh
+++ os/pacman.sh
@@ -5,5 +5,5 @@ git clone https://aur.archlinux.org/yay-git ~/.config/yay-git && (cd ~/.config/y
 yay -S picom-git
 yes | yay -S ttc-iosevka 
 
-yes | sudo pacman -Syyu && yes | sudo pacman -S man zsh tmux rxvt-unicode emacs polybar picom caja bluez bluez-utils pulseaudio-bluetooth unzip btop man-pages pavucontrolx feh
+yes | sudo pacman -Syyu && yes | sudo pacman -S man zsh tmux rxvt-unicode emacs polybar picom caja bluez bluez-utils pulseaudio-bluetooth unzip btop man-pages pavucontrolx feh rlwrap
 
