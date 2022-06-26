dd2e474a2ccf08386f332317ba9adb501af4e248
declan <declan-combley@protonmail.com>
2022-06-26 21:31:57 +0800

    fixed yes flag error

--- os/pacman.sh
+++ os/pacman.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+sudo pacman -Syyu && sudo pacman -S man-pages
+
 git clone https://aur.archlinux.org/yay-git ~/.config/yay-git && (cd ~/.config/yay-git/ && yes | makepkg -si) && echo "installed the aur"
 
 yay -S picom-git
 yes | yay -S ttc-iosevka 
 
-yes | sudo pacman -Syyu && yes | sudo pacman -S man zsh tmux rxvt-unicode emacs polybar picom caja bluez bluez-utils pulseaudio-bluetooth unzip btop man-pages pavucontrolx feh rlwrap
+sudo pacman -S man zsh tmux rxvt-unicode emacs polybar picom caja bluez bluez-utils pulseaudio-bluetooth unzip btop pavucontrol feh rlwrap
 
