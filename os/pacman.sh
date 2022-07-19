86f45a4f55bca70f0fb16788e4ae333b78d68b7e
declan <declan-combley@protonmail.com>
2022-07-19 11:14:36 +0800

    added latex dependencies

--- os/pacman.sh
+++ os/pacman.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-sudo pacman -Syyu && sudo pacman -S man-pages
+sudo pacman -Syyu
+sudo pacman -S man-pages texlive-most
 
 git clone https://aur.archlinux.org/yay-git ~/.config/yay-git && (cd ~/.config/yay-git/ && yes | makepkg -si) && echo "installed the aur"
 
 yay -S picom-git
 yes | yay -S ttc-iosevka 
 
-sudo pacman -S man zsh tmux rxvt-unicode emacs polybar picom caja bluez bluez-utils pulseaudio-bluetooth unzip btop pavucontrol feh rlwrap
+sudo pacman -S man zsh tmux rxvt-unicode emacs polybar picom caja bluez bluez-utils pulseaudio-bluetooth unzip btop pavucontrol feh rlwrap texlive-core
 
 curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
