b946ba1b4db218cfe4819eddc11cf8e767967fc4
declan <declan-combley@protonmail.com>
2022-07-19 14:43:20 +0800

    fix

--- os/pacman.sh
+++ os/pacman.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 sudo pacman -Syyu
-sudo pacman -S man-pages texlive-most
+sudo pacman -S man-pages
 
 git clone https://aur.archlinux.org/yay-git ~/.config/yay-git && (cd ~/.config/yay-git/ && yes | makepkg -si) && echo "installed the aur"
 
 yay -S picom-git
 yes | yay -S ttc-iosevka 
 
-sudo pacman -S man zsh tmux rxvt-unicode emacs polybar picom caja bluez bluez-utils pulseaudio-bluetooth unzip btop pavucontrol feh rlwrap texlive-core
+sudo pacman -S man zsh tmux rxvt-unicode emacs polybar picom caja bluez bluez-utils pulseaudio-bluetooth unzip btop pavucontrol feh rlwrap 
 
 curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
