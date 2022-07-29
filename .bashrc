10d4c45e7d92f4a3f8816cb6fb8412fc671b1c41
declan <declan-combley@protonmail.com>
2022-07-29 21:00:58 +0800

    added haskell support

--- .bashrc
+++ .bashrc
@@ -103,4 +103,6 @@ alias note='python3 ~/dotfiles/.prog/note/main.py'
 
 export PS1="[ \[$(tput sgr0)\]\[\033[38;5;215m\]\w\[$(tput sgr0)\] ] \[$(tput sgr0)\]\[\033[38;5;215m\]>>>\[$(tput sgr0)\] \[$(tput sgr0)\]"
 
+#[ -f "/home/declan/.ghcup/env" ] && source "/home/declan/.ghcup/env" # ghcup-env
+
 [ -f "/home/declan/.ghcup/env" ] && source "/home/declan/.ghcup/env" # ghcup-env
--- os/pacman.sh
+++ os/pacman.sh
@@ -8,6 +8,5 @@ git clone https://aur.archlinux.org/yay-git ~/.config/yay-git && (cd ~/.config/y
 yay -S picom-git
 yes | yay -S ttc-iosevka 
 
-sudo pacman -S man zsh tmux rxvt-unicode emacs polybar picom caja bluez bluez-utils pulseaudio-bluetooth unzip btop pavucontrol feh rlwrap 
+sudo pacman -S man zsh tmux rxvt-unicode emacs polybar picom caja bluez bluez-utils pulseaudio-bluetooth unzip btop pavucontrol feh rlwrap evince
 
-curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
