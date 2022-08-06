a300d6c2727cabc73ae12c8cce72ae1dd11ecb69
declan <declan-combley@protonmail.com>
2022-08-06 12:56:49 +0800

    update

--- os/pacman.sh
+++ os/pacman.sh
@@ -8,5 +8,5 @@ git clone https://aur.archlinux.org/yay-git ~/.config/yay-git && (cd ~/.config/y
 yay -S picom-git
 yes | yay -S ttc-iosevka 
 
-sudo pacman -S man zsh tmux rxvt-unicode emacs polybar picom caja bluez bluez-utils pulseaudio-bluetooth unzip btop pavucontrol feh rlwrap evince
+sudo pacman -S man zsh tmux rxvt-unicode emacs polybar picom caja bluez bluez-utils pulseaudio-bluetooth unzip btop pavucontrol feh rlwrap evince flameshot
 
