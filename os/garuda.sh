626caabd807dbf13e386c3a71eaef6c621c269d8
declan-combley <declan-combley@protonmail.com>
2023-04-23 01:22:49 +0800

    Logging In

---
+++ os/garuda.sh
@@ -0,0 +1,6 @@
+sudo pacman -S emacs vim 
+yes | yay -S ttc-iosevka
+
+sudo pacman -S bitwarden man rxvt-unicode emacs btop pavucontrol feh rlwrap man-pages noto-fonts
+ noto-fonts-extra noto-fonts-cjk 
+sudo pacman -FK
